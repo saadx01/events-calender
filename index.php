c832016c8d94a03c3ab88db7d801cee7ca4581c2
saadx01 <saadrehman1215@gmail.com>
2025-07-02 23:47:44 +0500

    Download Word button added, seperate func for calenderDataGenderation, and build created

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ function enqueue_vite_react_app() {
     // ✅ Replace with your actual Vite build filenames
     echo '<link rel="stylesheet" href="' . $plugin_url . 'assets/index-L6Qs19tw.css">';
     echo '<div id="root"></div>';
-    echo '<script type="module" src="' . $plugin_url . 'assets/index-BTq6dvCw.js"></script>';
+    echo '<script type="module" src="' . $plugin_url . 'assets/index-pLl-Vd5r.js"></script>';
 
     enqueue_scripts();
 }
